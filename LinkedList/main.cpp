4de6e9f603ee5d5ad2b48422d2704cacf20ae88b
giansg19 <giansg19@gmail.com>
2016-09-22 11:27:10 +0700

    Update main.cpp

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -19,9 +19,10 @@ int main()
     return 0;
 }
 
-void mainMenu(){
+void mainMenu()
+{
     /**
-	* IS : List sudah dibuat
+    * IS : List sudah dibuat
     * PR : memberikan output pilihan menu kepada user
     *       1. insert first
     *       2. insert after
@@ -33,8 +34,119 @@ void mainMenu(){
     *       8. insertion sort
     *       9. search
     */
-    //-------------your code here-------------
+    int pilihan;
+    address P1,P2,P;
+    bus busprec;
+    cout << "======MENU======"<<endl;
+    cout <<"1. insert first"<<endl;
+    cout <<"2. insert after"<< endl;
+    cout <<"3. insert last"<< endl;
+    cout <<"4. delete first"<<endl;
+    cout <<"5. delete after"<< endl;
+    cout <<"6. delete last"<< endl;
+    cout <<"7. view list"<< endl;
+    cout <<"8. insertion sort"<<endl;
+    cout <<"9. search"<<endl;
+    cout <<"================="<<endl;
+    cout <<"Pilihan : "<<endl;
+    cin >> pilihan;
 
-
-    //----------------------------------------
+    while (pilihan != 0)
+    {
+        switch (pilihan)
+    {
+    case 1 :
+    {
+        cout << "Masukkan data bus : "<<endl;
+        cout << "ID Bus : ";
+        cin >>  bus.id << endl;;
+        cout << "Brand Bus : ";
+        cin >> bus.brand<<endl;
+        cout << "Tipe Bus : ";
+        cin >> bus.type << endl;
+        cout << "Plat Bus : ";
+        cin >> bus.plat<<endl;
+        cout << "Kapasitas Bus : ";
+        cin >> bus.kapasitas << endl;
+        P1 = alokasi(bus);
+        insertFirst(L, P1);
+        cout << "Data telah berhasil ditambahkan" << endl <<endl;
+        break;
+    }
+    case 2 :
+    {
+        cout << "Masukkan data bus : "<<endl;
+        cout << "ID Bus : ";
+        cin >> bus.id << endl;;
+        cout << "Brand Bus : ";
+        cin >> bus.brand<<endl;
+        cout << "Tipe Bus : ";
+        cin >> bus.type << endl;
+        cout << "Plat Bus : ";
+        cin >> bus.plat<<endl;
+        cout << "Kapasitas Bus : ";
+        cin >> bus.kapasitas << endl;
+        cout << "Masukkan setelah ID Bus : ";
+        cin >> busprec<< endl;
+        P2 = findElm(busprec);
+        P1 = alokasi(bus);
+        insertAfter(P2,P1);
+        cout << "Data telah berhasil ditambahkan" << endl <<endl;
+        break;
+    }
+    case 3 :
+    {
+        cout << "Masukkan data bus : "<<endl;
+        cout << "ID Bus : ";
+        cin >> bus.id << endl;;
+        cout << "Brand Bus : ";
+        cin >> bus.brand<<endl;
+        cout << "Tipe Bus : ";
+        cin >> "bus.type" << endl;
+        cout << "Plat Bus : ";
+        cin >> bus.plat<<endl;
+        cout << "Kapasitas Bus : ";
+        cin >> bus.kapasitas << endl;
+        P1 = alokasi (bus);
+        insertLast(L, P1);
+        cout << "Data telah berhasil ditambahkan" << endl <<endl;
+    }
+    case 4 :
+        {
+            deleteFirst(L,P);
+            cout << "Data telah berhasil dihapus" << endl <<endl;
+        }
+    case 5 :
+        {
+        cout << "Masukkan data yang ingin dihapus : ";
+        cin >> bus;
+        P2 = findElm(bus);
+        P1 = alokasi(bus);
+        deleteAfter(P1,P2);
+        cout << "Data telah berhasil ditambahkan" << endl <<endl;
+        }
+    case 6 :
+        {
+            deleteLast(L,P);
+            cout << "Data telah berhasil dihapus" << endl <<endl;
+        }
+    case 7 :
+        {
+            printInfo(L);
+            cout << endl << endl;
+        }
+    case 8 :
+        {
+            insertionSort(L);
+        }
+    case 9 :
+        {
+            P1 = sentinelSearch(L, x);
+            cout << info(P1) << endl;
+        }
+    case 0 :
+        {
+        }
+    }
+    }
 }
